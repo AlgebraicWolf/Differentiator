7ee5c45e1260365ddf2bad7c8aae4e9233a9f0ac
Alexey <volkov.aa@phystech.edu>
2019-11-21 21:07:01 +0300

    Implemented file reading routine

--- main.cpp
+++ main.cpp
@@ -13,12 +13,43 @@ struct operation {
     char *latex;
 };
 
+char *loadFile(const char *filename);
+
+size_t getFilesize(FILE *f);
+
 int main() {
     printf("Loading equation...\n");
     return 0;
 }
 
 
-tree_t *loadEquation(char *filename, ) {
+tree_t *loadEquation(char *filename) {
+    char *serialized = loadFile(filename);
 
+    //Do loading routine
 }
+
+size_t getFilesize(FILE *f) {
+    assert(f);
+
+    fseek(f, 0, SEEK_END);
+    size_t size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    return size;
+}
+
+char *loadFile(const char *filename) {
+    assert(filename);
+
+    FILE *input = fopen(filename, "r");
+
+    size_t size = getFilesize(input);
+    char *content = (char *) calloc(size, sizeof(char));
+    fread(content, sizeof(char), size, input);
+
+    fclose(input);
+
+    return content;
+}
+
